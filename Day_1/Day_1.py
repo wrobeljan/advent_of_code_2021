64cf3611b55d9f3df34cb2907ebad7701e847230
wrobeljan <janek.wrob@gmail.com>
2021-12-04 00:03:34 +0100

    day 1 finished

--- Day_1/Day_1.py
+++ Day_1/Day_1.py
@@ -1,21 +1,36 @@
-measures=[int(line.strip()) for line in open('Day_1_input.txt')]
+measures = [int(line.strip()) for line in open('Day_1_input.txt')]
 
 '''with open('Day_1_input.txt') as inputFile:
     for i in inputFile:
         measures.append(int(i))
 '''
 
-count=-1;
+'''
 count = [count+1 for measure in measures if measure > measures[measures.index(measure)]]
 
 print(count)
 '''
-count=-1
-prev_measure=0
-for measure in measures:
-for
-    if measure>prev_measure: count+=1
-    prev_measure=measure
-print(count)
+
+def how_many_bigger(measures):
+    count = -1
+    prev_measure = 0
+    for measure in measures:
+        if measure > prev_measure: count += 1
+        prev_measure = measure
+    return count
+
+print(how_many_bigger(measures))
+
+'''
+places = [0, 1]
+for place in places:
+    measures.insert(place, 0)
 '''
 
+group_measures = []
+count_2=2
+for measure in range(count_2,len(measures)):
+    group_measures.append(measures[count_2]+measures[count_2-1]+measures[count_2-2])
+    count_2 += 1
+
+print(how_many_bigger(group_measures))
