05a10bc94245f110a483bc7eaba7da05c4d5f301
wrobeljan <janek.wrob@gmail.com>
2021-12-04 13:50:35 +0100

    finished with little bug

--- Day_3/Day_3.py
+++ Day_3/Day_3.py
@@ -1,86 +1,50 @@
-report = [line.strip() for line in open('Day_3_test_input.txt')]
+report = [line.strip() for line in open('Day_3_input.txt')]
 
-def string_change(old_string,place,value):
+
+def string_change(old_string, place, value):
     string_list = list(old_string)
     string_list[place] = value
     new_string = "".join(string_list)
     return new_string
 
-def gamma_epsilon(report):
-    sum = [0] * len(report[0])
-    half = len(report) / 2
+
+def gamma_epsilon(re):
+    su = [0] * len(re[0])
+    half = len(re) / 2
     rest = half % 1
-    for y in report:
-        for x in range(0, len(str(y))):
-            sum[x] = sum[x] + int(y[x])
     gamma_rate = ""
     epsilon_rate = ""
-    for i in sum:
-        if i>half:
-            gamma_rate = gamma_rate + "1"
-            epsilon_rate = epsilon_rate+"0"
+    for y in re:
+        for x in range(0, len(str(y))):
+            su[x] = su[x] + int(y[x])
+
+    for i in su:
+        if i > half:
+            gamma_rate += "1"
+            epsilon_rate += "0"
         else:
-            gamma_rate = gamma_rate + "0"
-            epsilon_rate = epsilon_rate + "1"
+            gamma_rate += "0"
+            epsilon_rate += "1"
     if rest == 0:
-        for i in range(0, len(sum)):
-            if sum[i] == half:
-                gamma_rate = string_change(gamma_rate,i,"1")
-                epsilon_rate = string_change(epsilon_rate,i,"0")
-    return [gamma_rate,epsilon_rate]
+        for i in range(0, len(su)):
+            if su[i] == half:
+                gamma_rate = string_change(gamma_rate, i, "1")
+                epsilon_rate = string_change(epsilon_rate, i, "0")
+    return [gamma_rate, epsilon_rate]
 
 
-gamma_rate=gamma_epsilon(report)[0]
-epsilon_rate=gamma_epsilon(report)[1]
+gamma = gamma_epsilon(report)[0]
+epsilon = gamma_epsilon(report)[1]
 
 print(gamma_epsilon(report))
-print(int(gamma_rate,2)*int(epsilon_rate,2))
-
-'''
-if rest==0:
-    for i in range(0,len(sum)):
-        if sum[i] == half:
-            gamma_rate[i]="1"
-            epsilon_rate[i]="1"
-
-print(gamma_rate, epsilon_rate)
-'''
+print(int(gamma, 2) * int(epsilon, 2))
 
 
-
-
-
-'''
-a=0
-while len(oxygen_generator) > 1:
-    for x in range(0, len(report[0])):
-        for y in oxygen_generator:
-            if y[x] != gamma_epsilon(oxygen_generator)[a][x]:
-                oxygen_generator.remove(y)
-
-
-a=1
-while len(co2_scrubber) > 1:
-    for x in range(0, len(report[0])):
-        criteria=gamma_epsilon(co2_scrubber)[a][x]
-        print(x)
-        print(criteria)
-        for y in range(0, len(co2_scrubber)):
-            print(co2_scrubber[y])
-            print(co2_scrubber[y][x])
-            if co2_scrubber[y][x] != criteria:
-                co2_scrubber.remove(co2_scrubber[y])
-                print(co2_scrubber)
-                
-
-          '''
-
-
-def ratio(rep,a):
+def ratio(rep, a):
     rating = rep
     while len(rating) > 1:
         for x in range(0, len(rep[0])):
-            criteria=gamma_epsilon(rating)[a][x]
+            criteria = gamma_epsilon(rating)[a][x]
             print(x)
             print(gamma_epsilon(rating)[a])
             print(criteria)
@@ -92,23 +56,19 @@ def ratio(rep,a):
                 print(rating[y][x])
 
                 if rating[y][x] != criteria:
-                    to_remove.insert(0,y)
+                    to_remove.insert(0, y)
                     print(to_remove)
-            for r in to_remove: rating.pop(r)
-            if len(rating) == 1:
-                return rating[0]
+            for r in to_remove:
+                rating.pop(r)
             print(rating)
             print("x")
+            if len(rating) == 1:
+                return rating[0]
 
-
-oxygen_generator,co2_scrubber=ratio(report,0),ratio(report,1)
-
-
-
+#oxygen_generator = ratio(report, 0)
+co2_scrubber = ratio(report, 1)
 
 #print(oxygen_generator)
 print(co2_scrubber)
-#print(int(oxygen_generator,2)*int(co2_scrubber,2))
-
-
-
+#print(int(oxygen_generator,2))
+print(int(co2_scrubber,2))
